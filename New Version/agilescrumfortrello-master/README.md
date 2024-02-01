70887290a1673c32f2ed06bf65e0842b8846a578
Charnyl Adaro <153164404+charnyl@users.noreply.github.com>
2024-02-01 10:21:28 +0800

    New Version

---
+++ New Version/agilescrumfortrello-master/README.md
@@ -0,0 +1,165 @@
+# Agile SCRUM for Trello
+
+
+![Agile SCRUM for Trello Chrome extension](https://user-images.githubusercontent.com/141241/110185336-655c7000-7e12-11eb-956c-153f3625ed4a.jpg)
+
+
+### Agile SCRUM for Trello boards gives super-powers to your Trello boards by enabling SCRUM features:
+
+- **STORY POINTS**: Set Story Points for Trello Cards
+- **TIME SPENT**: Set time spent on tasks.
+- **TAGS**: Group Cards into tags, User Stories or projects, these are colored automagically to save you time.
+- **PROGRESS BARS**: Visualize your Sprint progress instantly with unobstrusive background progress bars on both cards and lists.
+- **HEADER SEPARATORS**: Use header separators to group cards inside lists.
+- **VISUAL AIDS**: Cards with more Story Points have a slightly increased font size so you can distinguish bigger from smaller tasks at a glance.
+
+
+## Instructions
+
+There's no need to set up anything! This extension is made to be simple, install it and start giving super powers to your Trello.
+
+- Add Story Points to a card by typing the number in parenthesis:
+
+     "**(3)** Design new homepage header"
+
+- Track time spent on each card:
+
+     "**(1/3)** Design new homepage header"
+
+- Assign a card to a Project by writing it in square braquets:
+
+     "**[dev]** Implement Ads in footer"
+
+- Create a Header Separator by creating a new card with three asterisks at the start and end:
+
+     "*** Sprint 3 ***"
+
+You can add as many tags as needed. Tags will be colored automatically in a random color.
+
+Both Story Points and Tags can be defined anywhere in the card's text, it's usually best to add them at the start so it's easier to read.
+
+At the top right of the Lists and Cards you can see how many story points have been completed and how many there are in total.
+
+
+## Compatibility
+
+If a team member doesn't have the 'Agile SCRUM for Trello boards' extension installed he will still see be able to see Story Points and Projects as well as modify them (he won't see colors or progress bars). You can install/uninstall the extension at any time without loosing any data too.
+
+'Agile SCRUM for Trello boards' is the fastest and with the best performance of all similar extensions and at simply 2KB of code, a seamless extension to have installed, even for slow computers or for those users who want Chrome to work fast and are concerned on having too many extensions, this extension has such a tiny footprint that goes unnoticeable.
+
+
+## Links & Resources
+
+- Google Chrome Store: https://chrome.google.com/webstore/detail/agile-scrum-for-trello-bo/njmflagahgdhopbcdilgahjlfiecakpe?hl=en
+- Source code: https://github.com/luckyshot/agilescrumfortrello
+- Issue tracker: https://github.com/luckyshot/agilescrumfortrello/issues
+- Blog post + Screenshots: https://xaviesteve.com/5109/agile-scrum-trello-boards/
+
+
+## Installation
+
+You can download the published version in the [Google Chrome Store](https://chrome.google.com/webstore/detail/agile-scrum-for-trello-bo/njmflagahgdhopbcdilgahjlfiecakpe?hl=en) which is the easiest way yet it may be a few versions behind or:
+- [Download the code](https://github.com/luckyshot/agilescrumfortrello/archive/master.zip) (if you know how to use Git, you'll want to clone the repo somewhere in your computer instead of downloading)
+- In Google Chrome open the menu > _More Tools_ > _Extensions_
+- Check the _Developer mode_ checkbox
+- Drag and drop the _agilescrumfortrello_ folder (the one that has the _manifest.json_ file in it)
+- Reload the Trello page if already open
+
+
+## Screenshots
+
+![Agile SCRUM for Trello plugin](https://user-images.githubusercontent.com/141241/110185418-9472e180-7e12-11eb-84c3-63531df0f8e2.jpg)
+
+![Agile SCRUM for Trello](https://user-images.githubusercontent.com/141241/110185300-5249a000-7e12-11eb-9fe8-3821dddf2ea4.jpg)
+
+![Agile SCRUM for Trello browser extension](https://user-images.githubusercontent.com/141241/110185395-80c77b00-7e12-11eb-9e23-f3b6cee60f78.jpg)
+
+
+
+## Changelog
+
+#### 1.4.6
+- Several bugfixes and optimizations (thanks @Electrenator, @shacharmo)
+
+#### 1.4.5
+- Fixed Progress bar color not showing (thanks @danicg)
+- Custom tag colors by adding "red" or "#ff0" suffix (thanks @rudwolf)
+
+#### 1.4.4
+- Improved change detection through DOMSubtreeModified (thanks to JorgeBoscan)
+
+#### 1.4.3
+- Fixed issue where it wouldn't detect story points longer than 3 characters (0/10.5)
+- Changed method of detecting changes
+
+#### 1.4.2
+- Released Source Code under GPL license https://github.com/luckyshot/agilescrumfortrello
+- You can add the '#' symbol inside tags
+- Removed Settings button (not yet finished)
+
+#### 1.4.1
+- Rounded Story Points to one decimal
+
+#### 1.4.0
+- Huge refactoring for better scalability and future development of new features and feedback
+- When Story Points get overrun then show them in a slight Red color, when estimation was perfect then show it in slight Green color
+- Improved updates listener for better performance, responsiveness and less memory consumption
+
+#### 1.3.5
+- Fixed: 'innerHTML of null/undefined' bug
+- Increased randomness of Tag colors
+- Updated jQuery from 2.1.1 to 2.1.4
+
+#### 1.3.4
+- Fixed: Tag support for "." (dots)
+
+#### 1.3.3
+- FIXED: progress bar doesn't seem to work with themed backgrounds
+
+#### 1.3.2
+- Infinite decimals bug fixed
+
+#### 1.3.1
+- Can't remember, something awesome perhaps
+
+#### 1.3.0
+- Header Separators now count Story Points of tasks in them so you can have sub-groups inside each list (I use it to have archived Sprints in quarterly lists)
+- Better tag coloring (always nice smooth colors!)
+- Increased progress bars transparency for better readability
+
+#### 1.2.5
+- Fixed bug where Header Separators wouldn't show up when hyphens found
+
+#### 1.2.4
+- Header Separators
+
+#### 1.2.3
+- Fixed a bug where the extension would sometimes stop working when adding a new card
+
+#### 1.2.2
+- Progress bars colorize as a shade of the board background
+- Design improvements
+- Fixed a bug where Progress Bard wouldn't update correctly in some circumstances
+
+#### 1.2.1
+- Improved documentation
+- Code optimizations
+
+#### 1.2.0
+- Faster and smaller code for blazing performance and tiny footprint
+- Removed unnecessary files from extension pack by 20%
+
+#### 1.1.7
+- Automatically increase card's font size depending on Story Points
+- Code optimization and performance
+- Coded OOP design pattern
+
+#### 1.1.6
+- Added extension to Google Chrome Store
+- Bug fixes
+
+
+#### Note from the Developer
+
+_I am currently founding a startup which leads me to a very busy life. While I read all the issues and emails you guys send me, I can't reply to everyone. The project is Free and Open Sourced so anyone can participate, please feel free to submit any Pull Requests, I review and approve them quickly._
+
---
+++ New Version/agilescrumfortrello-master/agilescrumfortrello.js
@@ -0,0 +1,424 @@
+/*
+
+    Agile SCRUM for Trello Boards Google Chrome Extension
+    Copyright (C) 2015  Xavi Esteve
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+var AST = AST || {};
+
+(function( d ){ 'use strict';
+
+$( d ).ready(function(){
+
+AST = (function ( A ) {
+
+	var appName 				= 'Agile SCRUM for Trello boards',
+		appVersion				= '1.4.6',
+
+		regexFraction 			= /\([0-9\.]{1,6}\/[0-9\.]{1,6}\)/,
+		regexFractionDone 		= /\([0-9\.]{1,6}\//,
+		regexFractionTotal		= /\/[0-9\.]{1,6}\)/,
+		regexCheckboxDone		= /[0-9]{1,6}\//,
+		regexCheckboxTotal		= /\/[0-9]{1,6}/,
+		regexNumberTotal 		= /\([0-9\.]{1,5}\)/,
+		regexNumeric 			= /[0-9\.]+/,
+		regexProjectTag 		= /\[([\u3400-\uFAFF\u3040-\u30ff\uff65-\uffdca-zA-ZæÆåáãàâäéèẽêëíìĩîïøóòõôöúùũûüçÅÁÃÀÂÄÉÈẼÊËÍÌĨÎÏØÓÒÕÔÖÚÙŨÛÜÇ0-9 \_\-\.\#]*)\]/g,
+		regexHeader				= />\*{3} .+ \*{3}$/i,
+		regexShortLink			= /c\/([^/]+)\/.+/g,
+
+		storyPointDecimals 		= 1,
+
+		bodyColor 				= false,
+		bodyWidth				= 0,
+		cssStoryPoints			= '',
+
+		runTimer 				= false,
+		runTimerInterval 		= 5000,
+		runTimerChecksum 		= 0,
+
+		currentListElement		= false,
+		currentListDone			= 0,
+		currentListTotal		= 0,
+
+		currentHeaderElement	= false,
+		currentHeaderDone 		= 0,
+		currentHeaderTotal		= 0,
+
+		currentCardElement		= false,
+		currentCardDone 		= 0,
+		currentCardTotal 		= 0,
+		currentCardRemainder	= 0,
+		currentCardTitle 		= false,
+		currentCardFraction 	= false,
+
+		currentCheckListBadge	= false,
+		currentCheckFraction	= false,
+		currentCheckDone 		= 0,
+		currentCheckWeight		= 0,
+		currentCheckTotal		= 0;
+
+
+
+
+		/**
+		 * Initialize extension
+		 */
+		A.init = function()
+		{
+			console.info( appName + ' v' + appVersion + ' started' );
+			_start();
+			_run();
+		};
+
+
+		/**
+		 * Runs on page load only and gets things started
+		 */
+		var _start = function()
+		{
+			var cardId = null,
+				cardDetailOpen = null;
+
+			_run();
+
+			$('.window-wrapper').bind("DOMSubtreeModified",function(){
+			if ( window.location.href.match(regexShortLink) && !cardDetailOpen )
+			{
+				cardId = regexShortLink.exec( window.location.href )[1];
+				regexShortLink.lastIndex = 0;
+				cardDetailOpen = true;
+			}
+			else if ( !window.location.href.match( regexShortLink ) && cardDetailOpen )
+			{
+				cardDetailOpen = false;
+				_run();
+			}
+		});
+
+
+
+			runTimer = setInterval(function(){
+				_checkForChanges();
+			}, runTimerInterval );
+		};
+
+
+		/**
+		 * Counts the number of characters in div#board to see if anything changed
+		 * Not the most reliable method but better than the old one
+		 * See: https://github.com/luckyshot/agilescrumfortrello/issues/5
+		 */
+		var _checkForChanges =  function()
+		{
+			var currentChecksum = $('#board').html().length;
+
+			if ( runTimerChecksum !== currentChecksum )
+			{
+				runTimerChecksum = currentChecksum;
+				_run();
+			}
+		};
+
+
+		/**
+		 * Refreshes the whole page
+		 */
+		var _run = function()
+		{
+			// Get body color
+			bodyColor = ( $('#classic-body').css('background-color') && !$('#classic-body').hasClass('body-custom-board-background') ) ? $('#classic-body').css('background-color') : 'rgb(55, 158, 90)';
+
+			// Clean previous elements
+			_removeElements();
+
+			// Loop through Lists
+			$('#board').find('.list-cards').each(function(){
+
+				currentListElement = $(this);
+
+				// Loop through Cards
+				currentListElement.find('.list-card').each(function(){
+
+					currentCardElement = $(this);
+
+					// if the card is the textbox then ignore it
+					if ( _isCardATextBox( currentCardElement ) ) { return -2; }
+
+					// if the card is hidden then ignore it
+					if (currentCardElement.is(":hidden")) {
+						return -2;
+					}
+
+					// Reset title
+					if (  !$( currentCardElement ).find('.list-card-title').html().match(/<small /g) )
+					{
+						currentCardTitle = $( currentCardElement ).find('.list-card-title').html();
+						currentCardElement.data('original', currentCardTitle );
+					}
+					else
+					{
+						currentCardTitle = currentCardElement.data('original');
+						$( currentCardElement ).find('.list-card-title').html( currentCardTitle );
+					}
+
+
+
+					// Is Card a header?
+					if ( _isCardAHeader( currentCardTitle ) )
+					{
+						// If there's a previous header then process it and reset it
+						if ( currentHeaderTotal > 0 )
+						{
+							_finishHeader();
+						}
+						// Set as the current header
+						currentHeaderElement = currentCardElement;
+
+						// Style it (remove *** and add the CSS class)
+						currentHeaderElement[0].innerHTML = currentHeaderElement[0].innerHTML.replace(/( ?\*\*\* ?)/g, '');
+						currentHeaderElement.addClass( 'scrum-card-header' );
+					}
+
+
+					// Get card Story Points (fraction style)
+					else if ( currentCardTitle.match( regexFraction ) ) // (1/2)
+					{
+						currentCardFraction 	= currentCardTitle.match( regexFraction )[0];
+						currentCardDone 		= _getNumber( currentCardFraction.match( regexFractionDone )[0] );
+						currentCardTotal 		= _getNumber( currentCardFraction.match( regexFractionTotal )[0] );
+					}
+
+					// Get card Story Points (whole number style)
+					else if ( currentCardTitle.match( regexNumberTotal ) ) // (1)
+					{
+						currentCardTotal = _getNumber( currentCardTitle.match( regexNumberTotal )[0] );
+					}
+
+
+					// Get checklist badge
+					currentCheckListBadge = currentCardElement.find('.js-checkitems-badge');
+
+					// Check if card has checklist and fraction value is not overrun or perfect
+					if ( currentCardTotal >= currentCardDone && _hasCardAChecklist(currentCheckListBadge) )
+					{
+						// Get total checked and unchecked checkboxes
+						currentCheckFraction = currentCheckListBadge.find('.js-checkitems-badge-text')[0].innerHTML;
+						currentCheckDone = _getNumber( currentCheckFraction.match( regexCheckboxDone )[0] );
+						currentCheckTotal = _getNumber( currentCheckFraction.match( regexCheckboxTotal )[0] );
+
+						// Add checkbox done points with same weight to currentCartDone
+						currentCardRemainder = currentCardTotal - currentCardDone; // Keep fraction Story Points in mind
+						currentCheckWeight = currentCardRemainder / currentCheckTotal;
+						currentCardDone += currentCheckWeight * currentCheckDone;
+					}
+
+
+					// Card formatting
+
+					// Add red class if overrun
+					if ( currentCardDone == currentCardTotal )
+					{
+						cssStoryPoints = ' perfect';
+					}
+					else if ( currentCardDone > currentCardTotal )
+					{
+						cssStoryPoints = ' overrun';
+					}
+					else
+					{
+						cssStoryPoints = '';
+					}
+
+					// Display Story points
+					currentCardElement[0].innerHTML = currentCardElement[0].innerHTML.replace( regexNumberTotal, _formatCardPoints );
+					currentCardElement[0].innerHTML = currentCardElement[0].innerHTML.replace( regexFraction, _formatCardPoints );
+					currentCardElement[0].innerHTML = currentCardElement[0].innerHTML.replace( regexProjectTag, _formatCardProjectTag );
+
+					// display Progress bar
+					if ( currentCardTotal > 0 )
+					{
+						// Display Card progress bar
+						bodyWidth = currentCardDone / currentCardTotal * 100;
+						currentCardElement.prepend( '<div class="scrum-card-progress' + cssStoryPoints + '" style="background-color:' + bodyColor + ';width:' + ( bodyWidth <= 100 ? bodyWidth : 100 ) + '%"></div>');
+
+						// Increase card font size depending on its SP
+						$( currentCardElement[0] )
+							.css('font-size', ( (currentCardTotal < 8) ? (90 + (5*currentCardTotal) ) : 130 ) + '%' )
+							.css('line-height', '1.2em');
+					}
+
+
+
+					// Update list Story Points
+					currentListDone 	= currentListDone + currentCardDone;
+					currentListTotal 	= currentListTotal + currentCardTotal;
+
+
+					// Update header Story Points if there is a current one
+					if ( currentHeaderElement )
+					{
+						currentHeaderDone  = currentHeaderDone + currentCardDone;
+						currentHeaderTotal = currentHeaderTotal + currentCardTotal;
+					}
+
+					// now reset the Card for the next one
+					currentCardElement 	= false;
+					currentCardTitle 	= currentCardFraction 	= '';
+					currentCardTotal 	= currentCardDone 		= 0;
+
+
+				}); // end loop Cards
+
+
+
+
+				// last header
+				if ( currentHeaderTotal > 0 )
+				{
+					_finishHeader();
+				}
+
+				// display List Story Points
+				currentListElement.parent().prepend('<small class="scrum-list-total' + cssStoryPoints + '"><span class="scrum-light">' + currentListDone.toFixed( storyPointDecimals ) + '/</span>' + currentListTotal.toFixed( storyPointDecimals ) + '</small>');
+
+				// display List progress bar
+				if ( currentListTotal > 0 )
+				{
+					bodyWidth = currentListDone / currentListTotal * 100;
+					currentListElement.parent('.list').prepend('<div class="scrum-list-progress"  style="background-color:' + bodyColor + ';width:' + ( bodyWidth <= 100 ? bodyWidth : 100 ) + '%"></div>');
+				}
+
+				// now reset it for the next one
+				currentListElement 	= false;
+				currentListDone 	= currentListTotal 	= 0;
+
+			}); // end loop Lists
+
+		}; // _run
+
+
+		/**
+		 * Remove previously created elements
+		 */
+		var _removeElements = function()
+		{
+			$('.scrum-list-total,.scrum-list-progress,.scrum-card-progress').remove();
+		};
+
+
+		/**
+		 * Is this card the one with an input text (the last one)?
+		 */
+		var _isCardATextBox = function()
+		{
+			return currentCardElement.hasClass('js-composer');
+		};
+
+
+		/**
+		 * Checks the title for '***' '***'
+		 */
+		var _isCardAHeader = function( title )
+		{
+			return title.match( regexHeader );
+		};
+
+
+		/**
+		 * Checks if card has a checkbox
+		 */
+		var _hasCardAChecklist = function ( currentCardCheckListBadge )
+		{
+			return currentCardCheckListBadge.length === 1;
+		};
+
+
+		/**
+		 * Resets the header points and sets the header nicely
+		 */
+		var _finishHeader = function()
+		{
+			$( currentHeaderElement.find('.list-card-title')[0] ).prepend( '<small class="scrum-card-points'+((currentHeaderDone>currentHeaderTotal)?' overrun':'')+'"><span class="scrum-light">' + currentHeaderDone.toFixed( storyPointDecimals ) + '/</span>' + currentHeaderTotal.toFixed( storyPointDecimals ) + '</small>' );
+			currentHeaderDone = currentHeaderTotal = 0;
+		};
+
+
+		/**
+		 * Extract number from string
+		 */
+		var _getNumber = function( str )
+		{
+			return parseFloat( str.match( regexNumeric )[0] );
+		};
+
+
+		/**
+		 * Convert [project] strings to HTML
+		 */
+		var _formatCardProjectTag = function( match )
+		{
+			var matches = match.split("__");
+			var color = _stringToColor( matches[0] );
+			if (typeof matches[1] !== 'undefined')
+				color = matches[1];
+			return '<small class="scrum-card-project" style="background:' + color.replace( /\[|\]/g, '' ) + '">' + matches[0].replace( /\[|\]/g, '' ).toUpperCase() + '</small>';
+		};
+
+
+		/**
+		 * Convert (storypoint) strings to HTML
+		 */
+		var _formatCardPoints = function( match )
+		{
+			return '<small class="scrum-card-points' + cssStoryPoints + '">' + match.replace( /\(|\)/g, '' ).toUpperCase() + '</small>';
+		};
+
+
+		/**
+		 * Generate random color from a string
+		 */
+		var _stringToColor = function( s )
+		{
+			var r = 0,
+				i = 0,
+				len = s.length;
+
+			s = s.toLowerCase();
+
+			for ( ; i < len; i++ )
+			{
+				r += (s.charAt(i).charCodeAt() * 900);
+			}
+			return 'hsla('+(r % 256)+',50%,40%,1)';
+		};
+
+
+
+
+
+
+	return A;
+
+})( AST || {} );
+
+
+AST.init();
+
+
+}); // jQuery document ready
+
+}( document ));
